f1a6f7d8db4ad3876a776a9082b06e4b7372b620
Liam Brinston <brinston@ualberta.ca>
2020-08-12 13:58:01 -0700

    Revolved Sphinx error "...contents.rst not found" by designating master_doc = index.rst in conf.py

--- source/conf.py
+++ source/conf.py
@@ -29,6 +29,9 @@ author = 'Liam Brinston'
 # ones.
 extensions = []
 
+# Trying to resolve Sphinx error of contents.rst not found
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
